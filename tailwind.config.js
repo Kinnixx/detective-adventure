1b88ee3937df980b2b7838f22b1b2a0f8c7ab1c0
Kinnixx <anais.delattre1@gmail.com>
2025-03-17 15:26:38 +0100

    Initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}" // Ajoute ceci si tu as App Router
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
