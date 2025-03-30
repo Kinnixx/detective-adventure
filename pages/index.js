4adfd0c3541c24faeddc92f1e2a50bbd779affc9
Kinnixx <anais.delattre1@gmail.com>
2025-03-30 19:19:35 +0200

    feat: récupération des détails d'une scène

--- pages/index.js
+++ pages/index.js
@@ -20,9 +20,12 @@ export default function Home() {
 
   // Un choix peut être impossible à choisir si le joueur n'a pas la stat ou l'objet requis
   const isChoiceDisabled = (choice) => {
-    const lacksStat = choice.required_stat && stats[choice.required_stat.stat] < choice.required_stat_value;
-    const lacksObject = choice.required_item && !inventory.some(item => item.object.toLowerCase() === choice.required_item.toLowerCase());
-
+    const statName = choice.stat?.name?.toLowerCase();
+    const itemName = choice.item?.name?.toLowerCase();
+  
+    const lacksStat = statName && (stats[statName] ?? 0) < choice.required_stat_value;
+    const lacksObject = itemName && !inventory.some(item => item.object.toLowerCase() === itemName);
+  
     return lacksStat || lacksObject;
   }
 
@@ -31,48 +34,47 @@ export default function Home() {
       <HUD />
       <h1 className="text-2xl font-bold mt-4 sm:text-4xl lg:text-5xl leading-tight">Detective Quest 🕵️</h1>
 
-        {scene ? (
-            <>
-                <p className="mt-4 p-4 text-lg leading-relaxed sm:text-xl">{scene.text}</p>
-        
+      {scene ? (
+        <>
+        <p className="mt-4 p-4 text-lg leading-relaxed sm:text-xl">{scene.text}</p>
 
+        <div className="mt-6 flex flex-col gap-4">
+          { scene.choices.map((choice, index) => {
+            const isDisabled = isChoiceDisabled(choice);
 
-      <div className="mt-6 flex flex-col gap-4">
-        { scene.choices.map((choice, index) => {
-          const isDisabled = isChoiceDisabled(choice);
+            return (
+              <div>
+                <button key={index} 
+                  className={`px-4 py-3 text-sm sm:text-base w-full sm:w-auto rounded text-white ${isDisabled ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-700"}`} 
+                  onClick={() => {
+                    makeChoice(choice.next_scene_id, choice);
+                  }}
+                  disabled={isDisabled}
+                >
+                  {choice.text}
+                  
 
-          return (
-            <div>
-              <button key={index} 
-                className={`px-4 py-3 text-sm sm:text-base w-full sm:w-auto rounded text-white ${isDisabled ? "bg-gray-500 cursor-not-allowed" : "bg-blue-500 hover:bg-blue-700"}`} 
-                onClick={() => {
-                  makeChoice(choice.next_scene_id, choice);
-                }}
-                disabled={isDisabled}
-              >
-                {choice.text}
+                  {/* Affichage des éléments requis */}
+                  {choice.stat && (
+                    <span className={`text-sm ${isDisabled ? "text-red-400" : "text-green-400"} ml-2`}>
+                    (Requis : {choice.stat.name} {choice.required_stat_value}+)
+                  </span>                  
+                  )}
+                  {choice.item && (
+                    <span className={`text-sm ${isDisabled ? "text-red-400" : "text-green-400"} ml-2`}>
+                    (Requis : {choice.item.name})
+                  </span>                  
+                  )}
+                </button>
+              </div>
+            );
 
-                {/* Affichage des éléments requis */}
-                {choice.required_stat && (
-                  <span className={`text-sm ${isDisabled ? "text-red-400" : "text-green-400"} ml-2`}>
-                  (Requis : {choice.required_stat.stat} {choice.required_stat_value}+)
-                </span>                  
-                )}
-                {choice.required_item && (
-                  <span className={`text-sm ${isDisabled ? "text-red-400" : "text-green-400"} ml-2`}>
-                  (Requis : {choice.required_item})
-                </span>                  
-                )}
-              </button>
-            </div>
-          );
-
-        })}
-      </div>
-      </>
-      ) : (
-        <p>Chargement...</p>
-    )}
+          })}
+        </div>
+        </>
+        ) : (
+          <p>Chargement...</p>
+      )}
     </div>
   );
 }
